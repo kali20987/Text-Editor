951e492bddf2146737656b41151799061c05770d
kali19 <khadijah.syed.ali@gmail.com>
2023-05-31 18:40:02 -0400

    wasn't able to configure webpack or heroku

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Text-Editor
 This application is a text editor that runs in the browser. It is a single-page application that meets the PWA criteria.
+
+Deployed link: https://texteditorrr.herokuapp.com/ 
--- server/routes/htmlRoutes.js
+++ server/routes/htmlRoutes.js
@@ -2,5 +2,6 @@ const path = require('path');
 
 module.exports = (app) =>
   app.get('/', (req, res) =>
-    res.sendFile(path.join(__dirname, '../client/dist/index.html'))
+    //res.sendFile(path.join(__dirname, '/index.html'))
+    res.sendFile("/Users/jadeali/bootcamp/Text-Editor/client/index.html")
   );
